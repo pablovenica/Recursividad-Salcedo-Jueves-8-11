16b0b54ba60c722b6cc307bd8c539b7300e9f329
esalcedo86 <140828775+esalcedo86@users.noreply.github.com>
2023-08-15 10:57:49 -0300

    Initial commit

---
+++ Ej-05/digitos.cpp
@@ -0,0 +1,11 @@
+#include "digitos.h"
+
+#include <iostream>
+
+using namespace std;
+
+unsigned int cociente(unsigned int a, unsigned int b) { return 0; }
+
+unsigned int resto(unsigned int a, unsigned int b) { return 0; }
+
+void escribir_espaciado(unsigned int num) {}
